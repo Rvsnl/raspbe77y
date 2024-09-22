86f884b1194d2cfc67b502ee655edda02548f517
Rasul Abduragimov <rasul.abduragimov.03@mail.ru>
2024-09-22 18:14:39 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Rasul'
+print('Name: ' + name)
+age = 21
+print('Age: ' + str(age))
+age_new = age + 1
+print('New age: ' + str(age_new))
+is_student = True
+print('Is student: ' + str(is_student))
