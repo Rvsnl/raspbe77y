5d17dd6a0691ecffb746e92b4bbdfe7785ad8765
Rasul Abduragimov <rasul.abduragimov.03@mail.ru>
2024-09-22 17:49:15 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+name = 'Python'
+time = 1.5 / 12
+print('Курс: ' + name + ', всего задач: ' + str(homework) + ', затрачено часов: ' + str(hours) + ', среднее время выполнения ' + str(time) + ' часа.')
