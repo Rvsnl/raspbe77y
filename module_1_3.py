0fc03e4caf37796e2c9d443bb143eab8fc507ebe
Rasul Abduragimov <rasul.abduragimov.03@mail.ru>
2024-09-23 08:22:23 +0300

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Rasul'
 print('Name: ' + name)
 age = 21
 print('Age: ' + str(age))
-age_new = age + 1
-print('New age: ' + str(age_new))
+age = age + 1
+print('New age: ' + str(age))
 is_student = True
 print('Is student: ' + str(is_student))
